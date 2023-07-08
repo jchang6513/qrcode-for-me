483c3965e8bce47c504ae83ebaddf9479651deae
Yi-Wei Chang <jchang6513@gmail.com>
2023-07-08 13:33:30 +0800

    Apply style and action buttons

--- src/popup.js
+++ src/popup.js
@@ -7,8 +7,7 @@ const generateQRCode = (message) => {
     canvas,
     message,
     {
-      margin: 1,
-      width: 220,
+      width: 240,
     },
     (error) => {
       if (error) console.error(error)
@@ -17,9 +16,46 @@ const generateQRCode = (message) => {
   );
 }
 
+const toggleTooltip = (event, tip) => {
+  tooltip.style.display = "block";
+  tooltip.style.left = `${event.clientX}px`;
+  tooltip.style.top = `${event.clientY - 25}px`;
+  tooltip.innerText = tip;
+  setTimeout(() => {
+    tooltip.style.display = "none";
+  }, 1000)
+}
+
+const copyQRCode = (event) => {
+  const tooltip = document.getElementById("tooltip");
+
+  canvas.toBlob(function(blob) {
+    const item = new ClipboardItem({ "image/png": blob });
+
+    navigator.clipboard.write([item])
+      .then(function() {
+        toggleTooltip(event, 'COPIED!');
+      })
+      .catch(function(error) {
+        toggleTooltip(event, 'FAILED!');
+      });
+  });
+}
+
+const downloadQRCode = () => {
+  const downloadLink = document.createElement("a");
+  const dataURL = canvas.toDataURL("image/png");
+
+  downloadLink.href = dataURL;
+  downloadLink.download = "qrcode.png";
+  downloadLink.click();
+}
+
 (function () {
   const canvas = document.getElementById('canvas')
   const messageText = document.getElementById('message')
+  const copyBtn = document.getElementById('copy')
+  const downloadBtn = document.getElementById('download')
 
   chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
     const currentTab = tabs[0];
@@ -32,17 +68,7 @@ const generateQRCode = (message) => {
     generateQRCode(event.currentTarget.value);
   });
 
-  canvas.addEventListener("dblclick", function() {
-    canvas.toBlob(function(blob) {
-      const item = new ClipboardItem({ "image/png": blob });
-
-      navigator.clipboard.write([item])
-        .then(function() {
-          console.log("Image copied to clipboard successfully.");
-        })
-        .catch(function(error) {
-          console.error("Unable to copy image to clipboard:", error);
-        });
-    });
-  });
+  canvas.addEventListener('dblclick', copyQRCode);
+  copyBtn.addEventListener('click', copyQRCode);
+  downloadBtn.addEventListener('click', downloadQRCode);
 })();
